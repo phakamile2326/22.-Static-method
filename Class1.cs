5e0a9d23c9d5e0c04790ecfda92a8b7cc504a73e
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-10 13:59:51 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_method
+{
+    static internal class UseFullTools
+    {
+        public static void sayHi(string name) 
+        {
+          Console.WriteLine("Hi" + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//static method is about the class itself, no need to create objects to get the class itself
+         //C# already has ,amy prelude classes like math class that we can call upon whenever without having to make objects
+
+            Console.WriteLine(Math.Sqrt(144));
+
+            //UsefulTools tools = new UseFullTools(), if you make class static as well, you will only access it with direct methods,
+            //no objects just like you can't make an object of a Math class
+
+            UseFullTools.sayHi("Phaka");
+            Console.ReadLine();
+        }
+    }
+}
